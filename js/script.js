326650a2ee552ec9a3723d5970e72543c5bb5ce5
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-09-16 14:11:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,10 +10,10 @@ async function callApi() {
         const response = await fetch(url);
         const json = await response.json();
         console.log(json);
-        createHtml(json.data);
+        createHtml(json.data, ".result-container");
     } catch (error) {
         console.log(error);
-        resultContainer.innerHTML = displayMessage("error", error);
+        displayMessage("error", error, ".result-container");
     }
 }
 
